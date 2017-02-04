341da5a8561d3e4bfcfc26a0c63957bb230d3183
ppseprus <p.seprus@gmail.com>
2017-02-04 03:27:29 +0100

    Add script to modify the image filter

---
+++ enhance.js
@@ -0,0 +1,51 @@
+const maximumBrightness = 30;
+const maximumContrast = 15;
+
+const setFitlers = (brightness, contrast) => {
+	document.getElementsByClassName('tile')[0]
+		.setAttribute('style', `filter: brightness(${brightness}) contrast(${contrast});`);
+};
+
+document.onmousemove = e => {
+	if (document.getElementsByClassName('tile').length !== 1) {
+		return;
+	}
+
+	let satellite = document.getElementsByClassName('tile-loader')[0],
+		tileSize = parseFloat(getComputedStyle(satellite).width) / 2,
+		range = tileSize / 2,
+		tileMidX = satellite.getBoundingClientRect().left + tileSize,
+		tileMidY = satellite.getBoundingClientRect().top + tileSize,
+		dx = e.clientX - tileMidX,
+		dy = e.clientY - tileMidY;
+
+	const scale = (value, boundary) => {
+		return 1 + (value / (range / boundary)) / 100;
+	};
+
+    if (Math.abs(dx) <= range && Math.abs(dy) <= range) {
+    	let brightness = scale(dx, maximumBrightness),
+    		contrast = scale(dy, maximumContrast);
+
+    	setFitlers(brightness, contrast);
+    } else {
+
+    	setFitlers(1, 1);
+    }
+};
+
+let observer = new MutationObserver(mutations => {
+	mutations.forEach(mutation => {
+		if (document.getElementsByClassName('tile').length !== 1) {
+			return;
+		}
+
+		if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+			setFitlers(1, 1);
+		}
+
+	});
+});
+
+observer.observe(document, { subtree: true, attributes: true });
+
